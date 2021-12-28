15e3e207ab4ce66ef93f8459c2a5d8702d73b6b0
Lucas <rokstargamesbr@gmail.com>
2021-12-28 19:32:49 -0300

    v1.7

--- cadastrar.php
+++ cadastrar.php
@@ -9,6 +9,16 @@ $senha = mysqli_real_escape_string($conexao, trim(md5($_POST['senha'])));
 $sql = "select count(*) as total from usuario where usuario = '$usuario'";
 $result = mysqli_query($conexao, $sql);
 $row = mysqli_fetch_assoc($result);
+$mat = mysqli_fetch_assoc($result);
+
+$consultaRegistro = "SELECT * FROM usuario WHERE usuario = '{$usuario}'";
+
+if($row['total'] == 1) {
+	$_SESSION['usuario_existe'] = true;
+	header('Location: register-form.php');
+	exit;
+}
+
 
 if($row['total'] == 1) {
 	$_SESSION['usuario_existe'] = true;
