c6015efa79d6e9eb004cf445b975c237a5c4af3a
Lucas <rokstargamesbr@gmail.com>
2022-02-02 02:53:15 -0300

    version 1.6.9

--- js/app.js
+++ js/app.js
@@ -74,5 +74,8 @@ $(document).ready(function(){
 });
 
 
-
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
  
